d1854bcfb0bfd2defd9a92bd3faee873f1962071
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 16:54:05 +0400

    Corrections

--- src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleModelService.java
+++ src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleModelService.java
@@ -1,11 +1,10 @@
 package ru.egartech.vehicleapp.service.interfaces;
 
-import ru.egartech.vehicleapp.api.request.VehicleBrandRequest;
 import ru.egartech.vehicleapp.api.request.VehicleModelRequest;
 import ru.egartech.vehicleapp.model.VehicleModel;
 
 public interface VehicleModelService {
     VehicleModel create(VehicleModelRequest request);
-    VehicleBrandRequest findByhModelName(VehicleModelRequest request);
+    VehicleModel findByModelName(VehicleModelRequest request);
 
 }
