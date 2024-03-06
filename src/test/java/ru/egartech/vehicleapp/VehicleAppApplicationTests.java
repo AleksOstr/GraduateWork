407acb34d7eadb31168b54831f22693b29d4fa9f
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 14:43:10 +0400

    Initial commit

---
+++ src/test/java/ru/egartech/vehicleapp/VehicleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egartech.vehicleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
