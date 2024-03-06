0871001f941b5d80e7e73919c50f3eab523e1dfb
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 15:30:40 +0400

    Model repository created

---
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleModelRepository.java
@@ -0,0 +1,11 @@
+package ru.egartech.vehicleapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.egartech.vehicleapp.model.VehicleModel;
+
+import java.util.UUID;
+
+@Repository
+public interface VehicleModelRepository extends JpaRepository<VehicleModel, UUID> {
+}
