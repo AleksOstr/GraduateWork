07215ec5dcd436362faa51c01a374a602cbb39aa
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 16:35:08 +0400

    New find methods

--- src/main/java/ru/egartech/vehicleapp/repository/VehicleBrandRepository.java
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleBrandRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egartech.vehicleapp.model.VehicleBrand;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VehicleBrandRepository extends JpaRepository<VehicleBrand, UUID> {
+    List<VehicleBrand> findAllByBrandNameIgnoreCase(String brandName);
+    Optional<VehicleBrand> findByBrandNameIgnoreCase(String brandName);
 }
--- src/main/java/ru/egartech/vehicleapp/repository/VehicleModelRepository.java
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleModelRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egartech.vehicleapp.model.VehicleModel;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VehicleModelRepository extends JpaRepository<VehicleModel, UUID> {
+    Optional<VehicleModel> findByModelNameIgnoreCase(String modelName);
 }
