5487700bc0af129047077f5164bcbba3e2503640
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 18:13:13 +0400

    New find method

--- src/main/java/ru/egartech/vehicleapp/repository/VehicleCategoryRepository.java
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleCategoryRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egartech.vehicleapp.model.VehicleCategory;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VehicleCategoryRepository extends JpaRepository<VehicleCategory, UUID> {
+    Optional<VehicleCategory> findByCategoryNameIgnoreCase(String categoryName);
 }
