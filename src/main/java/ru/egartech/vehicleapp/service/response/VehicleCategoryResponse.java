be0e8e26caf90bae5077d687fb8f6191c6d7605d
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 18:11:06 +0400

    Response created

---
+++ src/main/java/ru/egartech/vehicleapp/service/response/VehicleCategoryResponse.java
@@ -0,0 +1,8 @@
+package ru.egartech.vehicleapp.service.response;
+
+import lombok.Data;
+
+@Data
+public class VehicleCategoryResponse {
+    private String categoryName;
+}
---
+++ src/main/java/ru/egartech/vehicleapp/service/response/VehicleModelResponse.java
@@ -0,0 +1,8 @@
+package ru.egartech.vehicleapp.service.response;
+
+import lombok.Data;
+
+@Data
+public class VehicleModelResponse {
+    private String modelName;
+}
