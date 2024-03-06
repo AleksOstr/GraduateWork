8e0eca88389508357716932804c39a416daf527d
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 14:43:50 +0400

    Initial operations

--- src/main/java/ru/egartech/vehicleapp/VehicleAppApplication.java
+++ src/main/java/ru/egartech/vehicleapp/VehicleAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VehicleAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleAppApplication.class, args);
+    }
 
 }
