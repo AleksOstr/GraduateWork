c1a1c0aeb575b75dafc2e1938377464a3a7fa0e3
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 20:01:15 +0400

    Optimize imports

--- src/main/java/ru/egartech/vehicleapp/repository/VehicleTypeRepository.java
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleTypeRepository.java
@@ -3,9 +3,7 @@ package ru.egartech.vehicleapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egartech.vehicleapp.model.VehicleType;
-import ru.egartech.vehicleapp.service.response.VehicleTypeResponse;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
