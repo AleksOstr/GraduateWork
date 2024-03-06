19db1779a4d795fb012890dc7c5959b889e9bac7
AleksOstr <ostryankoam@yandex.ru>
2024-03-06 20:00:05 +0400

    Type service implementation

--- src/main/java/ru/egartech/vehicleapp/repository/VehicleTypeRepository.java
+++ src/main/java/ru/egartech/vehicleapp/repository/VehicleTypeRepository.java
@@ -3,9 +3,13 @@ package ru.egartech.vehicleapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.egartech.vehicleapp.model.VehicleType;
+import ru.egartech.vehicleapp.service.response.VehicleTypeResponse;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VehicleTypeRepository extends JpaRepository<VehicleType, UUID> {
+    Optional<VehicleType> findByTypeNameIgnoreCase(String name);
 }
