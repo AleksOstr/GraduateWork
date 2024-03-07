ddd4713fc072ccb75bcaba48866092841f094ab9
AleksOstr <ostryankoam@yandex.ru>
2024-03-07 18:37:26 +0400

    deleted

--- src/main/java/ru/egartech/vehicleapp/api/interfaces/VehicleBrandController.java
+++
@@ -1,8 +0,0 @@
-package ru.egartech.vehicleapp.api.interfaces;
-
-import ru.egartech.vehicleapp.api.request.VehicleBrandRequest;
-
-public interface VehicleBrandController {
-    String create(VehicleBrandRequest  request);
-    String findByBrandName(VehicleBrandRequest request);
-}
