e5a01e54786aa0a932c870955bbed5c94c46d370
AleksOstr <ostryankoam@yandex.ru>
2024-03-08 18:16:51 +0400

    Exception handlers added

--- src/main/java/ru/egartech/vehicleapp/api/VehicleController.java
+++ src/main/java/ru/egartech/vehicleapp/api/VehicleController.java
@@ -4,9 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.egartech.vehicleapp.api.request.SearchRequest;
 import ru.egartech.vehicleapp.api.request.VehicleRequest;
+import ru.egartech.vehicleapp.exceptions.ExistingValueException;
+import ru.egartech.vehicleapp.exceptions.ValueNotFoundException;
 import ru.egartech.vehicleapp.service.interfaces.VehicleService;
 import ru.egartech.vehicleapp.service.response.VehicleResponse;
 
@@ -61,5 +64,19 @@ public class VehicleController {
     public String updateVehicle(@PathVariable String regNumber, @Valid VehicleRequest request) {
         service.update(request, regNumber);
         return "redirect:/vehicle";
+    }
+
+    @ExceptionHandler(ExistingValueException.class)
+    public String handleException(ExistingValueException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleException(MethodArgumentNotValidException e, Model model) {
+        String message = "Допускаются только государственные номера РФ формата А000АА000 (кириллица, " +
+                "заглавные символы АВЕКМНОРСТУХ)";
+        model.addAttribute("message", message);
+        return "error";
     }
 }
