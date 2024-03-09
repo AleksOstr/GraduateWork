c56249af354345ce5a022f11346e437816c36f94
AleksOstr <ostryankoam@yandex.ru>
2024-03-09 19:41:51 +0400

    Comments added

--- src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleModelService.java
+++ src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleModelService.java
@@ -2,11 +2,30 @@ package ru.egartech.vehicleapp.service.interfaces;
 
 import ru.egartech.vehicleapp.model.VehicleModel;
 
+/**
+ * Сервис моделей ТС
+ */
 public interface VehicleModelService {
+
+    /**
+     * Создание новой модели ТС
+     * @param brandName - имя марки ТС
+     * @param modelName - имя модели ТС
+     * @return - VehicleModel сущность модели ТС
+     */
     VehicleModel create(String brandName, String modelName);
 
+    /**
+     * Поиск модели ТС по имени
+     * @param modelName - имя модепли ТС
+     * @return - VehicleModel сущность модели ТС
+     */
     VehicleModel findByName(String modelName);
 
+    /**
+     * Обновление модели ТС
+     * @param model - модель ТС
+     */
     void updateModel(VehicleModel model);
 
 }
