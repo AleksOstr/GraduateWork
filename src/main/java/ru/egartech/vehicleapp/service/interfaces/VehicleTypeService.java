708d6b99c31affae1fedaa1e14a8c7782a5dbbf2
AleksOstr <ostryankoam@yandex.ru>
2024-03-09 19:49:41 +0400

    Comments added

--- src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleTypeService.java
+++ src/main/java/ru/egartech/vehicleapp/service/interfaces/VehicleTypeService.java
@@ -5,12 +5,34 @@ import ru.egartech.vehicleapp.service.response.VehicleTypeResponse;
 
 import java.util.List;
 
+/**
+ * Сервис типов ТС
+ */
 public interface VehicleTypeService {
+
+    /**
+     * Создание нового типа ТС
+     * @param typeName - имя типа ТС
+     * @return - VehicleType - сущность типа ТС
+     */
     VehicleType create(String typeName);
 
+    /**
+     * Пооиск типа ТС по имени
+     * @param typeName - имя типа ТС
+     * @return - VehicleType - сущность типа ТС
+     */
     VehicleType findByName(String typeName);
 
+    /**
+     * Получение всех типов ТС из БД
+     * @return List<VehicleTypeResponse> - список типов ТС
+     */
     List<VehicleTypeResponse> findAll();
 
+    /**
+     * Оновление типа ТС
+     * @param type - тип ТС
+     */
     void updateType(VehicleType type);
 }
