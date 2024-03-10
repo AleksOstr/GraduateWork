f6226691899b58aa25890578836922527f7630c5
AleksOstr <ostryankoam@yandex.ru>
2024-03-10 11:59:51 +0400

    Comments added

--- src/main/java/ru/egartech/vehicleapp/service/VehicleBrandServiceImpl.java
+++ src/main/java/ru/egartech/vehicleapp/service/VehicleBrandServiceImpl.java
@@ -10,11 +10,20 @@ import ru.egartech.vehicleapp.service.interfaces.VehicleBrandService;
 
 import java.util.ArrayList;
 
+/**
+ * Сервис марок ТС
+ */
 @Service
 @RequiredArgsConstructor
 public class VehicleBrandServiceImpl implements VehicleBrandService {
 
     private final VehicleBrandRepository brandRepository;
+
+    /**
+     * Создание новой марки
+     * @param brandName - имя марки ТС
+     * @return VehicleBrand - сущность марки ТС
+     */
     @Override
     public VehicleBrand create(String brandName) throws ExistingValueException {
         if (brandRepository.findByNameIgnoreCase(brandName).isPresent()) {
@@ -28,12 +37,21 @@ public class VehicleBrandServiceImpl implements VehicleBrandService {
         return brandRepository.save(brand);
     }
 
+    /**
+     * Поиск марки по имени
+     * @param brandName - имя марки ТС
+     * @return VehicleBrand - сущность марки ТС
+     */
     @Override
     public VehicleBrand findByName(String brandName) throws ValueNotFoundException{
         return brandRepository.findByNameIgnoreCase(brandName)
                 .orElseThrow(() -> new ValueNotFoundException("Brand with name: " + brandName + " not found"));
     }
 
+    /**
+     * Обновление марки в репозитории
+     * @param brand - сущность марки ТС
+     */
     @Override
     public VehicleBrand updateBrand(VehicleBrand brand) {
         return brandRepository.save(brand);
