f54e5fae27ae337f6882d797c3aefbf8375c10da
AleksOstr <ostryankoam@yandex.ru>
2024-03-10 15:56:15 +0400

    findByNameExceptionTest() added

--- src/test/java/ru/egartech/vehicleapp/service/VehicleBrandServiceTest.java
+++ src/test/java/ru/egartech/vehicleapp/service/VehicleBrandServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.egartech.vehicleapp.exceptions.ExistingValueException;
+import ru.egartech.vehicleapp.exceptions.ValueNotFoundException;
 import ru.egartech.vehicleapp.model.VehicleBrand;
 import ru.egartech.vehicleapp.repository.VehicleBrandRepository;
 import ru.egartech.vehicleapp.service.interfaces.VehicleBrandService;
@@ -57,6 +58,14 @@ public class VehicleBrandServiceTest {
         Assertions.assertEquals(saved.getName(), found.getName());
 
         repository.delete(saved);
+    }
+
+    @Test
+    void findByNameExceptionTest() {
+        Exception exception = Assertions.assertThrows(ValueNotFoundException.class, () -> service.findByName("name"));
+        String message = exception.getMessage();
+
+        Assertions.assertEquals("Brand with name: name not found", message);
     }
 
     @Test
