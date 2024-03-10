ea4a84df65e54123990a6b9cc6324e2d404ed196
AleksOstr <ostryankoam@yandex.ru>
2024-03-10 12:15:54 +0400

    Comments added

--- src/main/java/ru/egartech/vehicleapp/service/response/VehicleTypeResponse.java
+++ src/main/java/ru/egartech/vehicleapp/service/response/VehicleTypeResponse.java
@@ -2,6 +2,9 @@ package ru.egartech.vehicleapp.service.response;
 
 import lombok.Data;
 
+/**
+ * response-объект с параметрами типа ТС
+ */
 @Data
 public class VehicleTypeResponse {
     private String typeName;
