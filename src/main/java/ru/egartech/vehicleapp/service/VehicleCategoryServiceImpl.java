27e852707e56190ca9066430e5970cbe029184f8
AleksOstr <ostryankoam@yandex.ru>
2024-03-10 12:00:34 +0400

    Comments added

--- src/main/java/ru/egartech/vehicleapp/service/VehicleCategoryServiceImpl.java
+++ src/main/java/ru/egartech/vehicleapp/service/VehicleCategoryServiceImpl.java
@@ -10,13 +10,20 @@ import ru.egartech.vehicleapp.service.interfaces.VehicleCategoryService;
 
 import java.util.ArrayList;
 
+/**
+ * Сервис категорий ТС
+ */
 @Service
 @RequiredArgsConstructor
 public class VehicleCategoryServiceImpl implements VehicleCategoryService {
 
     private final VehicleCategoryRepository categoryRepository;
 
-
+    /**
+     * Создание новой категории ТС
+     * @param categoryName - имя катеогрии ТС
+     * @return VehicleCategory - сущность категории ТС
+     */
     @Override
     public VehicleCategory create(String categoryName) throws ExistingValueException {
         if (categoryRepository.findByNameIgnoreCase(categoryName).isPresent()) {
@@ -29,13 +36,21 @@ public class VehicleCategoryServiceImpl implements VehicleCategoryService {
         return categoryRepository.save(category);
     }
 
+    /**
+     * Поиск категории ТС по имени
+     * @param categoryName - имя катеогрии ТС
+     * @return VehicleCategory - сущность категории ТС
+     */
     @Override
     public VehicleCategory findByName(String categoryName) throws ValueNotFoundException {
         return categoryRepository.findByNameIgnoreCase(categoryName)
                 .orElseThrow(() -> new ValueNotFoundException("Category with name: " + categoryName + " not found"));
     }
 
-
+    /**
+     * Обновление категории ТС в репозитории
+     * @param category - категория ТС
+     */
     @Override
     public VehicleCategory updateCategory(VehicleCategory category) {
         return categoryRepository.save(category);
