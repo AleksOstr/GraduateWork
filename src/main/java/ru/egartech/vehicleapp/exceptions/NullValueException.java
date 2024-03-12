a3788060dff77349c934296833bed8171cd1d123
AleksOstr <ostryankoam@yandex.ru>
2024-03-12 13:29:52 +0400

    deleted

--- src/main/java/ru/egartech/vehicleapp/exceptions/NullValueException.java
+++
@@ -1,7 +0,0 @@
-package ru.egartech.vehicleapp.exceptions;
-
-public class NullValueException extends RuntimeException{
-    public NullValueException(String message) {
-        super(message);
-    }
-}
